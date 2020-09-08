4b215defba22041c035421b5daaa97dea09dd362
goodrichbrett <brettgoodrich@outlook.com>
2020-09-08 10:59:56 -0500

    new navbar

--- routes/songs.js
+++ routes/songs.js
@@ -2,11 +2,11 @@ const router = require('express').Router();
 const songsCtrl = require('../controllers/songs');
 
 router.get('/', isLoggedIn, songsCtrl.index);
-router.post('/search', songsCtrl.search);
-router.get('/new', songsCtrl.newSong);
+router.post('/search', isLoggedIn, songsCtrl.search);
+router.get('/new', isLoggedIn, songsCtrl.newSong);
 router.get('/:trackId', isLoggedIn, songsCtrl.show);
-router.post('/:trackId/owned', songsCtrl.addToOwned);
-router.delete('/:trackId/owned', songsCtrl.removeFromList);
+router.post('/:trackId/owned', isLoggedIn, songsCtrl.addToOwned);
+router.delete('/:trackId/owned', isLoggedIn, songsCtrl.removeFromList);
 
 function isLoggedIn(req, res, next) {
 	if (req.isAuthenticated()) return next();
